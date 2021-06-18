8bdaf144efe31131131b2939ef434c095dabb2f5
museun <museun@outlook.com>
2021-06-18 15:25:37 -0400

    adjust visibility of exports

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,18 @@
-pub mod app;
-pub use app::App;
-pub mod ui;
-pub use ui::{build_ui, MessagesView, OnView};
-pub mod state;
-pub use state::AppState;
 pub mod colors;
-pub mod entry;
+
 pub mod panic_logger;
 pub mod twitch;
 
+mod app;
+pub use app::App;
+
+mod ui;
+pub use ui::{build_ui, MessagesView, OnView};
+
+mod state;
+pub use state::AppState;
+
 mod config;
 pub use config::{Badges as ConfigBadges, Color as ConfigColor, Config, Style as ConfigStyle};
+
+mod entry;
