fbaf9cda5f039f9ee208d49b26dc46d984e12961
museun <museun@outlook.com>
2021-06-18 22:41:19 -0400

    minor cleanups

--- src/app.rs
+++ src/app.rs
@@ -108,25 +108,5 @@ impl App {
     }
 }
 
-trait FindView {
-    fn seek_and_toggle<T, F>(&mut self, key: &str, extract: F)
-    where
-        T: View,
-        F: Fn(&mut Config) -> &mut bool;
-}
-
-impl FindView for Cursive {
-    fn seek_and_toggle<T, F>(&mut self, key: &str, extract: F)
-    where
-        T: View,
-        F: Fn(&mut Config) -> &mut bool,
-    {
-        let show = {
-            let config = &mut *get_config();
-            let show = extract(config);
-            *show = !*show;
-            *show
-        };
-        self.call_on_all_named(key, |view: &mut HideableView<T>| view.set_visible(show))
-    }
-}
+mod find_view;
+pub use find_view::FindView;
---
+++ src/app/find_view.rs
@@ -0,0 +1,26 @@
+use cursive::{views::HideableView, Cursive, View};
+
+use crate::{get_config, Config};
+
+pub trait FindView {
+    fn seek_and_toggle<T, F>(&mut self, key: &str, extract: F)
+    where
+        T: View,
+        F: Fn(&mut Config) -> &mut bool;
+}
+
+impl FindView for Cursive {
+    fn seek_and_toggle<T, F>(&mut self, key: &str, extract: F)
+    where
+        T: View,
+        F: Fn(&mut Config) -> &mut bool,
+    {
+        let show = {
+            let config = &mut *get_config();
+            let show = extract(config);
+            *show = !*show;
+            *show
+        };
+        self.call_on_all_named(key, |view: &mut HideableView<T>| view.set_visible(show))
+    }
+}
